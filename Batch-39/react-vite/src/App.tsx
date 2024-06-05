41038a40f3ab733d4b80fa0be9b2e42e46196cf7
nhan <nhannn87dn@gmail.com>
2024-06-05 21:03:02 +0700

    ss9

--- Batch-39/react-vite/src/App.tsx
+++ Batch-39/react-vite/src/App.tsx
@@ -1,17 +1,28 @@
 import "./App.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import ProductsReactQuery from "./components/ProductsReactQuery";
 //import Categories from "./components/Categories";
-import CategoriesAxios from "./components/CategoriesAxios";
+//import CategoriesAxios from "./components/CategoriesAxios";
+
+const queryClient = new QueryClient();
 
 function App() {
   console.log("app render");
 
   return (
-    <div className="container mx-auto">
-      {/* <h2>Categories Fetch</h2>
-      <Categories /> */}
-      <h2>CategoriesAxios</h2>
-      <CategoriesAxios />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container mx-auto">
+        {/* <h2>Categories Fetch</h2>
+        <Categories /> */}
+        {/* <h2>CategoriesAxios</h2>
+        <CategoriesAxios /> */}
+        <ProductsReactQuery />
+      </div>
+    </QueryClientProvider>
   );
 }
 
