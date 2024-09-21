6bc5c5ef5f0f7613939481a232e29250f02d13d6
nhan <nhannn87dn@gmail.com>
2024-09-21 15:29:55 +0700

    off 3

--- Batch-186-188/react-vite/src/App.tsx
+++ Batch-186-188/react-vite/src/App.tsx
@@ -1,76 +1,22 @@
 import "./App.css";
-import React from "react";
 
-function ButtonLike({
-  liked,
-  onHandleClick,
-}: {
-  liked: boolean;
-  onHandleClick: () => void;
-}) {
-  console.log(liked);
-  return (
-    <button onClick={onHandleClick} className={liked ? "text-blue-500" : ""}>
-      Thích
-    </button>
-  );
+function ButtonDemo({ label }: { label: string }) {
+  return <button className="btn btn-primary">{label}</button>;
 }
 
-function Count() {
-  //let index = 0;
-  //useState(0) Có nghĩa giá trị khởi tạo của index lúc đầu = 0
-  const [index, setIndex] = React.useState(0); //Hook
+// function sum({a,b}: {a: number, b: number}){
+//   console.log( a + b)
+// }
 
-  function handleClick() {
-    //index = index + 1;
-    setIndex(index + 1);
-  }
-  return (
-    <>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          console.log("Đã click");
-          handleClick();
-        }}
-      >
-        Increment
-      </button>
-      <h3>{index}</h3>
-    </>
-  );
-}
+// sum(2,4) = 6
+// sum(4,4) = 8
 
 function App() {
-  const [isLike, setIsLike] = React.useState<boolean>(false); // false là chưa click
-  const [isOpen, setIsOpen] = React.useState(false); // false có nghĩa chưa mở
-  console.log("App render");
   return (
     <>
-      <h1>State</h1>
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        className="btn btn-primary"
-      >
-        Add To Cart
-      </button>
-      {isOpen && (
-        <div className="modal">
-          <p>Thêm vào giỏ hàng thành công</p>
-        </div>
-      )}
-      <hr />
-      <ButtonLike
-        onHandleClick={() => {
-          console.log("đã click");
-          setIsLike(!isLike);
-        }}
-        liked={isLike}
-      />{" "}
-      <br />
-      <Count />
+      <h1>Components</h1>
+      <ButtonDemo label={"Them vao gio hang"} />
+      <ButtonDemo label={"Goi Lai Tu Va"} />
     </>
   );
 }
--- Batch-186-188/react-vite/src/components/ButtonAddToCard/index.tsx
+++ Batch-186-188/react-vite/src/components/ButtonAddToCard/index.tsx
@@ -1,12 +1,13 @@
 import { FaShoppingCart } from "react-icons/fa";
 import styles from "./ButtonAddToCart.module.css";
+import './ButtonAddToCart.css'
 
 const ButtonAddToCard = () => {
   console.log(styles);
 
   return (
     <>
-      <button className={styles.button}>
+      <button className={styles.button_primary}>
         <FaShoppingCart /> Thêm vào giỏ hàng
       </button>
     </>
