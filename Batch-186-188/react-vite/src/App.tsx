e0fca69a4a4acc47138a8ecaac83f9e9c731c5d2
nhan <nhannn87dn@gmail.com>
2024-09-24 18:28:21 +0700

    hwss6

--- Batch-186-188/react-vite/src/App.tsx
+++ Batch-186-188/react-vite/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import CreateAccountForm from "./components/CreateAccountForm";
 import LoginFormReactHook from "./components/LoginFormReactHook";
 import SimpleForm from "./components/SimpleForm";
 
@@ -7,6 +8,8 @@ function App() {
 
   return (
     <div className="container mx-auto">
+      <h2>Create Account Form</h2>
+      <CreateAccountForm />
       <h2>React Hook Form</h2>
       <LoginFormReactHook />
       <SimpleForm />
