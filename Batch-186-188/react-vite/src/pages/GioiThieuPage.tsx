44dec27e29063670d836197bbcd8bb28d73c2c74
nhan <nhannn87dn@gmail.com>
2024-10-05 20:26:39 +0700

    ss12

--- Batch-186-188/react-vite/src/pages/GioiThieuPage.tsx
+++ Batch-186-188/react-vite/src/pages/GioiThieuPage.tsx
@@ -1,16 +1,54 @@
 import React from "react";
-import { useProfileStore } from "../stores/useProfileStore";
-const GioiThieuPage = () => {
-  const { user, isLoading, fetchProfile } = useProfileStore();
+import "./GioiThieu.css";
+import axios from "axios";
 
-  React.useEffect(() => {
-    fetchProfile(); //gọi hàm để lấy thông tin user
-  }, []);
+type TWeather = {
+  location: {
+    name: string;
+  };
+  current: {
+    temp_c: number;
+  };
+};
+const GioiThieuPage = () => {
+  const [city, setCity] = React.useState("da nang");
+  console.log("<<=== 🚀 city ===>>", city);
+  const [weather, setWeather] = React.useState<TWeather | null>(null);
+
+  const onHanldeSubmitCity = async (e) => {
+    e.preventDefault();
+    //city
+    //di goi API thoi tiet theo city da nhap
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=c9a0ca46550648b29ce125849232709&q=${city}&aqi=no&lang=vi`
+    );
+    console.log("<<=== 🚀 data ===>>", response.data);
+    setWeather(response.data);
+  };
 
   return (
-    <div>
-      <h1>GioiThieuPage</h1>
-      {isLoading ? <div>Loading...</div> : <div>{user?.name}</div>}
+    <div className="flex justify-center">
+      <div className="iphone_size">
+        <form onSubmit={onHanldeSubmitCity}>
+          <input
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            name="city"
+            value={city}
+            type="text"
+          />
+          <button type="submit" className="btn btn-default">
+            Search
+          </button>
+        </form>
+
+        {weather && (
+          <>
+            <p>{weather.current.temp_c} C</p>
+          </>
+        )}
+      </div>
     </div>
   );
 };
