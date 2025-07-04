1a0940bb6220257ab20deeda4cb81236fc974aa9
nhannn87dn <nhannn87dn@gmail.com>
2025-07-04 20:11:16 +0700

    ss2

--- Batch-194/react-vite-typescript/src/App.tsx
+++ Batch-194/react-vite-typescript/src/App.tsx
@@ -1,26 +1,22 @@
 import "./App.css";
-import ButtonA from "./components/ButtonA";
-import ButtonB from "./components/ButtonB";
-import { AtSign } from "lucide-react";
-import { BrushCleaning } from "lucide-react";
-/*
-Code một component có tên là HelloReact
-Hiển thị ra ngoài: Hello React !
-*/
-
+import Button from "./components/Button";
+import { ShoppingCart, Heart } from "lucide-react";
 function App() {
+  const handleClickButton = () => {
+    console.log("Clicked");
+  };
   return (
     <div>
-      <AtSign />{" "}
-      <BrushCleaning style={{ color: "red" }} height={40} width={40} />
-      <img height={150} src="images/vivo.jpg" alt="" />
-      <img
-        height={150}
-        src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/7264/231776/elio-es045-01-nam-thumb-fix-600x600.jpg"
-        alt=""
+      <Button
+        onHandleClick={handleClickButton}
+        icon={<ShoppingCart />}
+        label="Thêm vào giỏ hàng"
       />
-      <ButtonA />
-      <ButtonB />
+      <Button
+        onHandleClick={handleClickButton}
+        icon={<Heart />}
+        label={"Yêu thích"}
+      />
     </div>
   );
 }
---
+++ Batch-194/react-vite-typescript/src/components/Homework-SS4/BlockUI1.tsx
@@ -0,0 +1,5 @@
+const BlockUI1 = () => {
+  return <div>BlockUI1</div>;
+};
+
+export default BlockUI1;
