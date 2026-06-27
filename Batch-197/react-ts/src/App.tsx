c06ee20ddc3ff7dd2d91b2d1653c7240327f4373
nhannn87dn <nhannn87dn@gmail.com>
2026-06-27 18:39:51 +0700

    hw-ss4

--- Batch-197/react-ts/src/App.tsx
+++ Batch-197/react-ts/src/App.tsx
@@ -1,36 +1,11 @@
 import "./App.css";
-import Basic1 from "./components/homeworks/session03/Basic1";
-//import Basic1 from "./components/homeworks/session03/Basic1";
-import BlockUI1 from "./components/homeworks/session03/BlockUI1";
-import Videos from "./components/Videos";
+import Exercise7Homeworks4 from "./components/homeworks/session04/Exercise7Homeworks4";
 
-//cách 1: if đơn
-// function App() {
-//   const isShow = true;
-
-//   if (!isShow) return null;
-
-//   return (
-//     <>
-//       <BlockUI1 />
-//     </>
-//   );
-// }
-
-//Cách 2: toán tử 3 ngôi
-// function App() {
-//   const isShow = true;
-//   return <>{isShow ? <BlockUI1 /> : null}</>;
-// }
-
-//Cách 3: sử dụng toán tử &&
 function App() {
-  const isShow = true;
   return (
     <>
-      <Videos />
-      {isShow && <BlockUI1 />}
-      <Basic1 />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Exercise7Homeworks4 />
     </>
   );
 }
---
+++ Batch-197/react-ts/src/components/homeworks/session04/Exercise7Homeworks4.tsx
@@ -0,0 +1,122 @@
+const data = {
+  top: [
+    {
+      id: 1,
+      name: "top 1",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 2,
+      name: "top 2",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 1,
+      name: "top 3",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+  ],
+  best: [
+    {
+      id: 1,
+      name: "best 1",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 2,
+      name: "best 2",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 1,
+      name: "best 3",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+  ],
+  lasted: [
+    {
+      id: 1,
+      name: "lasted 1",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 2,
+      name: "lasted 2",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+    {
+      id: 1,
+      name: "lasted 3",
+      price: 49,
+      thumbnail: "images/nokia.jpg",
+    },
+  ],
+};
+
+type TProduct = {
+  id: number;
+  name: string;
+  price: number;
+  thumbnail: string;
+};
+
+const ProductItem = ({ product }: { product: TProduct }) => {
+  return (
+    <div className="product-item flex gap-x-2">
+      <div className="thumbnail w-15 overflow-hidden">
+        <img
+          className="w-full h-auto"
+          src={product.thumbnail}
+          alt={product.name}
+        />
+      </div>
+      <div className="info flex-1">
+        <h3>{product.name}</h3>
+        <div className="price">
+          <strong>{product.price}</strong>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Exercise7Homeworks4 = () => {
+  return (
+    <div className="recommend-wrapper max-w-xl mx-auto flex gap-x-3">
+      <div className="top-products flex-1">
+        <h2 className="font-bold uppercase mb-5">Top Products</h2>
+        <div className="top-product-list flex flex-col gap-y-5">
+          {data.top.map((product) => {
+            return <ProductItem product={product} />;
+          })}
+        </div>
+      </div>
+      <div className="top-products  flex-1">
+        <h2 className="font-bold uppercase  mb-5">Best Products</h2>
+        <div className="best-product-list flex flex-col gap-y-5">
+          {data.best.map((product) => {
+            return <ProductItem product={product} />;
+          })}
+        </div>
+      </div>
+      <div className="top-products  flex-1">
+        <h2 className="font-bold uppercase  mb-5">Lasted Products</h2>
+        <div className="best-product-list flex flex-col gap-y-5">
+          {data.lasted.map((product) => {
+            return <ProductItem product={product} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Exercise7Homeworks4;
