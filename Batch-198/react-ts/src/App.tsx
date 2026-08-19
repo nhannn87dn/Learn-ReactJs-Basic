ce39b19c937a7cf2909df3b2b994c72b5642a377
nhannn87dn <nhannn87dn@gmail.com>
2026-08-19 15:06:29 +0700

    fetch product

--- Batch-198/react-ts/src/App.tsx
+++ Batch-198/react-ts/src/App.tsx
@@ -1,35 +1,13 @@
 import "./App.css";
-import ReactHookFormBasic from "./components/ReactHookFormBasic";
-import ReactHookFormValidation from "./components/ReactHookFormValidation";
-import SubscriberForm from "./components/SubscriberForm";
-// import FormExample from "./components/FormExample";
+import FetchProduct from "./components/FetchProduct";
 
 function App() {
   return (
     <main className="container">
-      {/* <FormExample /> */}
-      <ReactHookFormBasic />
-      <hr />
-      <ReactHookFormValidation />
-      <hr />
-      <SubscriberForm />
+      <h1>Fetch API</h1>
+      <FetchProduct />
     </main>
   );
 }
 
 export default App;
-
-/*
-TASK 1;
-Tạo một component HeartButton để mô phỏng lại
-Nút thả tim như Zalo.
-- Mặc định lúc đầu tim màu đen
-- Click vào thì nó thành màu đỏ
-
-=======
-Task 2
-- Tạo một components SuggestSearch gồm 2 phần
-+ Input text và một thẻ div với nội dung "Tìm kiếm gần đây"
-- Mặc định thẻ div chưa xuất hiện
-- Khi click vào input thì thẻ div xuất hiện.
-*/
---
+++ Batch-198/react-ts/src/components/FetchProduct.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import styles from "./FetchProduct.module.css";
+//bước 1: Tạo một kiểu dữ liệu cho sản phẩm
+type TProduct = {
+  id: number;
+  title: string;
+  price: number;
+  thumbnail: string;
+};
+
+const FetchProduct = () => {
+  // Bước 2: Sử dụng useState để lưu trữ dữ liệu sản phẩm
+  const [products, setProducts] = useState<TProduct[]>([]);
+  //3. Sử dụng useEffect để gọi API khi component được render lần đầu tiên
+  useEffect(() => {
+    //4. Gọi API để lấy dữ liệu sản phẩm
+    //Tạo một hàm async để gọi API
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json(); //covert json to object
+        console.log("<<=== 🚀 response ===>>", response);
+        console.log("<<=== 🚀 data ===>>", data.products);
+        //Bước 5:Sau khi lấy được dữ liệu thì đi cập nhật cho state product
+        if (response.status === 200) {
+          setProducts(data.products);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    //Gọi hàm fetchProduct để lấy dữ liệu sản phẩm
+    fetchProduct();
+  }, []); //Nếu tham số thứ 2 là một mảng rổng, thì callback chỉ chạy 1 lần
+
+  console.log("<<=== 🚀 products ===>>", products);
+  return (
+    <div className={styles.products_list}>
+      {products.length > 0 &&
+        products.map((product) => {
+          return (
+            <div key="{products.id}" className={styles.product_item}>
+              <div className={styles.thumbnail}>
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="w-full h-48 object-cover"
+                />
+              </div>
+              <div className={styles.product_info}>
+                <h3 className={styles.product_name}>{product.title}</h3>
+                <div className={styles.product_price}>
+                  <strong>${product.price}</strong>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default FetchProduct;
