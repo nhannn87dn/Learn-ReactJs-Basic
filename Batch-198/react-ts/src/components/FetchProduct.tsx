013da1fe2ceedd22cd4a02bbe0600076ffae93fe
nhannn87dn <nhannn87dn@gmail.com>
2026-08-19 15:59:52 +0700

    done phase 1

--- Batch-198/react-ts/src/components/FetchProduct.tsx
+++ Batch-198/react-ts/src/components/FetchProduct.tsx
@@ -11,12 +11,16 @@ type TProduct = {
 const FetchProduct = () => {
   // Bước 2: Sử dụng useState để lưu trữ dữ liệu sản phẩm
   const [products, setProducts] = useState<TProduct[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   //3. Sử dụng useEffect để gọi API khi component được render lần đầu tiên
   useEffect(() => {
     //4. Gọi API để lấy dữ liệu sản phẩm
     //Tạo một hàm async để gọi API
     const fetchProduct = async () => {
       try {
+        //bắt đầu gọi thì set isLoading = true
+        setIsLoading(true);
         const response = await fetch("https://dummyjson.com/products");
         const data = await response.json(); //covert json to object
         console.log("<<=== 🚀 response ===>>", response);
@@ -27,6 +31,10 @@ const FetchProduct = () => {
         }
       } catch (error) {
         console.log(error);
+        setIsError(true);
+      } finally {
+        //kết thúc gọi thì set isLoading = false
+        setIsLoading(false);
       }
     };
     //Gọi hàm fetchProduct để lấy dữ liệu sản phẩm
@@ -34,6 +42,14 @@ const FetchProduct = () => {
   }, []); //Nếu tham số thứ 2 là một mảng rổng, thì callback chỉ chạy 1 lần
 
   console.log("<<=== 🚀 products ===>>", products);
+
+  if (isError) {
+    return <div className={styles.error}>Lỗi về lấy dữ liệu</div>;
+  }
+
+  if (isLoading) {
+    return <div className={styles.loading}>Đang tải dữ liệu...</div>;
+  }
   return (
     <div className={styles.products_list}>
       {products.length > 0 &&
@@ -41,11 +57,7 @@ const FetchProduct = () => {
           return (
             <div key="{products.id}" className={styles.product_item}>
               <div className={styles.thumbnail}>
-                <img
-                  src={product.thumbnail}
-                  alt={product.title}
-                  className="w-full h-48 object-cover"
-                />
+                <img src={product.thumbnail} alt={product.title} />
               </div>
               <div className={styles.product_info}>
                 <h3 className={styles.product_name}>{product.title}</h3>
